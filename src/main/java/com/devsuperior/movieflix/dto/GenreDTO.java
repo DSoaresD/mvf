90832242e154dbe903a54fbe0b04c48b745f4a29
DSoaresD <danielsoares321@gmail.com>
2022-09-02 23:54:58 -0300

    second part

--- src/main/java/com/devsuperior/movieflix/dto/GenreDTO.java
+++ src/main/java/com/devsuperior/movieflix/dto/GenreDTO.java
@@ -1,8 +1,72 @@
 package com.devsuperior.movieflix.dto;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import com.devsuperior.movieflix.entities.Genre;
 
 public class GenreDTO implements Serializable{
 	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+	private String name;
+	private List<MovieDTO> movies = new ArrayList<>();
+	
+	public GenreDTO() {
+		
+	}
+
+	public GenreDTO(Long id, String name) {
+		this.id = id;
+		this.name = name;
+		
+	}
+	
+	public GenreDTO(Genre entity) {
+		this.id = entity.getId();
+		this.name = entity.getName();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public List<MovieDTO> getMovies() {
+		return movies;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, movies, name);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		GenreDTO other = (GenreDTO) obj;
+		return Objects.equals(id, other.id) && Objects.equals(movies, other.movies) && Objects.equals(name, other.name);
+	}
+
+	
+	
 
 }
